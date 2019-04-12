4872288caabf363d2ba9fa5e6db308d18fe304e4
Paleless <1505672421@qq.com>
2019-04-12 21:09:32 +0800

    4/12

--- markdown/markdown_parse.exs
+++ markdown/markdown_parse.exs
@@ -8,30 +8,51 @@ defmodule MarkdownParse do
     |> read()
     |> parse()
     |> save(filename)
-
-    # read the file
-    # parse the file to html
-    ## split content into lines and parse lines for rules like (# ## ### > --- ***)
-    ## split line into words and parse word for rules like (`` **)
-    # save the file
   end
 
+  @doc """
+  read the file
+  """
   def read(filename) do
     case File.read(filename) do
       {:ok, body} ->
         body
 
       {:error, info} ->
-        IO.puts("oops! does it exist?")
+        IO.puts("oops! does it exist? try again.\n")
         IO.inspect(info)
+        start()
     end
   end
 
+  @doc """
+  split the content by line and then parse line into html
+  at last join tegother and return 
+  """
   def parse(body) do
     body
+    |> String.split(~r/\r|\n|\r\n/, trim: true)
+    |> Enum.map(&parse_line/1)
+    |> Enum.join("\n")
   end
 
-  def to_html(text) do
+  @doc """
+  match the md rule and convert to html
+  """
+  def parse_line(line) do
+    case Regex.run(~r/^(#|##|###|####|#####|######|-)\s(.*)/, line) do
+      [_, "#", content] -> "<h1>" <> content <> "</h1>"
+      [_, "##", content] -> "<h2>" <> content <> "</h2>"
+      [_, "###", content] -> "<h3>" <> content <> "</h3>"
+      [_, "####", content] -> "<h4>" <> content <> "</h4>"
+      [_, "#####", content] -> "<h5>" <> content <> "</h5>"
+      [_, "######", content] -> "<h6>" <> content <> "</h6>"
+      [_, "-", content] -> "<li>" <> content <> "</li>"
+      [_, ">", content] -> "<li>" <> content <> "</li>"
+      [_, "---", content] -> "<br/>"
+      [_, "***", content] -> "<br/>"
+      _ -> line
+    end
   end
 
   def save(content, filename) do
@@ -46,6 +67,40 @@ defmodule MarkdownParse do
         IO.inspect(info)
     end
   end
-end
 
-MarkdownParse.start()
+  def p(c) do
+    String.replace(c, ~r/(\r\n|\r|\n)(.*)\1/, "<p>\\1</p>")
+  end
+
+  def italics(c) do
+    String.replace(c, ~r/[^\*]*\*(.*)\*[^\*]*/, "<em>\\1</em>")
+  end
+
+  def bold(c) do
+    String.replace(c, ~r/\*\*(.*)\*\*/, "<strong>\\1</strong>")
+  end
+
+  def h1(c) do
+    String.replace(c, ~r/#\s+(.*)/, "<h1>\\1</h1>")
+  end
+
+  def h2(c) do
+    String.replace(c, ~r/#{2}\s+(.*)/, "<h2>\\1</h2>")
+  end
+
+  def h3(c) do
+    String.replace(c, ~r/#{3}\s+(.*)/, "<h3>\\1</h3>")
+  end
+
+  def h4(c) do
+    String.replace(c, ~r/#{4}\s+(.*)/, "<h4>\\1</h4>")
+  end
+
+  def h5(c) do
+    String.replace(c, ~r/#{5}\s+(.*)/, "<h5>\\1</h5>")
+  end
+
+  def h6(c) do
+    String.replace(c, ~r/#{6}\s+(.*)/, "<h6>\\1</h6>")
+  end
+end
